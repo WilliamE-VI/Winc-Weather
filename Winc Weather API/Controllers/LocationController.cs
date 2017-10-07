cfff0c3c0a109bb8479d36505580b4792b89325f
William <will285.59@gmail.com>
2017-10-07 16:11:58 -0700

    Added Location API and Model

---
+++ Winc Weather API/Controllers/LocationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Winc_Weather_API.Models;
+
+namespace Winc_Weather_API.Controllers
+{
+    [RoutePrefix("api/location")]
+    public class LocationController : ApiController
+    {
+        [HttpPost]
+        [Route("Create")]
+        public void Create(Location location)
+        {
+
+        }
+    }
+}
