0d91ab3f12e6b2282b0b1593495dd55fbd3de4a6
William <will285.59@gmail.com>
2017-10-08 01:37:39 -0700

    Added edmx for entity framework connection to database Removed Models folder and contents Added all the endpoints to LocationController Added LocationAccess class for data access

--- Winc Weather API/Controllers/LocationController.cs
+++ Winc Weather API/Controllers/LocationController.cs
@@ -1,16 +1,54 @@
 ﻿using System.Web.Http;
-using Winc_Weather_API.Models;
+using Winc_Weather_API.DAL;
+using RestSharp;
+using System.Collections.Generic;
 
 namespace Winc_Weather_API.Controllers
 {
     [RoutePrefix("api/location")]
     public class LocationController : ApiController
     {
+        public const string APPID = "4ce152a3a00e4a17bf10c3e0233689be";
+
+        [HttpGet]
+        [Route("GetWeather")]
+        public object GetWeather(int locationId)
+        {
+            var location = LocationAccess.GetLocationById(locationId);
+
+            var client = new RestClient("http://api.openweathermap.org/data/2.5/");
+            var request = new RestRequest("forecast?zip=" + location.Zipcode + ",us&appid=" + APPID);
+
+            var stuff = client.Execute(request);
+            return stuff;
+        }
+
+        [HttpGet]
+        [Route("GetByZip")]
+        public Location GetByZip(string zip)
+        {
+            return LocationAccess.GetLocationByZip(zip);
+        }
+
+        [HttpGet]
+        [Route("GetByName")]
+        public List<Location> GetByName(string name)
+        {
+            return LocationAccess.GetLocationsByName(name);
+        }
+
         [HttpPost]
         [Route("Create")]
         public void Create(Location location)
         {
+            LocationAccess.AddLocation(location);
+        }
 
+        [HttpDelete]
+        [Route("DeleteById")]
+        public void DeleteById(int locationId)
+        {
+            LocationAccess.DeleteLocation(locationId);
         }
     }
 }
---
+++ Winc Weather API/DAL/LocationAccess.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Winc_Weather_API.DAL
+{
+    public static class LocationAccess
+    {
+        public static Location GetLocationByZip(string zip)
+        {
+            using (var db = new WincWeatherEntities())
+            {
+                var locations = from location in db.Locations
+                                where location.Zipcode == zip
+                                select location;
+                return locations.FirstOrDefault();
+            }
+        }
+
+        public static List<Location> GetLocationsByName(string name)
+        {
+            using (var db = new WincWeatherEntities())
+            {
+                var locations = from location in db.Locations
+                                where location.Name == name
+                                select location;
+                return locations.ToList();
+            }
+        }
+
+        public static Location GetLocationById(int locationId)
+        {
+            using (var db = new WincWeatherEntities())
+            {
+                var locations = from location in db.Locations
+                                where location.Id == locationId
+                                select location;
+                return locations.FirstOrDefault();
+            }
+        }
+
+        public static void AddLocation(Location newLocation)
+        {
+            using (var db = new WincWeatherEntities())
+            {
+                db.Locations.Add(newLocation);
+                db.SaveChanges();
+            }
+        }
+
+        public static void DeleteLocation(int id)
+        {
+            using (var db = new WincWeatherEntities())
+            {
+                var removedLocation = GetLocationById(id);
+                if (removedLocation != null)
+                { 
+                    db.Locations.Attach(removedLocation);
+                    db.Locations.Remove(removedLocation);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
